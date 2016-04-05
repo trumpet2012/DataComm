2d3a6771542e299c0d97150a44d6b06c93087bc8
Trent Holliday <tholliday@morristechnology.com>
2016-04-05 10:31:26 -0400

    Updating wsgi config.

--- DataComm/wsgi.py
+++ DataComm/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DataComm.settings")
-
+sys.path.insert(0, '/opt/python/current/app')
 application = get_wsgi_application()
