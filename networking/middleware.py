a771094f6026adf109893e9994860db8abebcd27
Trent Holliday <tholliday@morristechnology.com>
2016-04-07 16:23:13 -0400

    Removing right most proxy setting to see what happens.

--- networking/middleware.py
+++ networking/middleware.py
@@ -4,8 +4,11 @@ from ipware.ip import get_real_ip, get_ip
 class IpMiddleware:
 
     def process_request(self, request):
-        ip = get_real_ip(request, right_most_proxy=True)
+        ip = get_real_ip(request)
         if ip is None:
             ip = get_ip(request=request)
+            print "Non real IP: %s" % ip
+        else:
+            print "Real IP: %s" % ip
 
         request.ip = ip
