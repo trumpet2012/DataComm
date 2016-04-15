e2edb363afe1c99c5c655002f5515ab1422a27ed
Trent Holliday <tholliday@morristechnology.com>
2016-04-14 21:57:31 -0400

    Switching to useing right most proxy for testing.

--- networking/middleware.py
+++ networking/middleware.py
@@ -4,8 +4,8 @@ from ipware.ip import get_real_ip, get_ip
 class IpMiddleware:
 
     def process_request(self, request):
-        ip = get_real_ip(request)
+        ip = get_real_ip(request, right_most_proxy=True)
         if ip is None:
-            ip = get_ip(request=request)
+            ip = get_ip(request=request, right_most_proxy=True)
 
         request.ip = ip
